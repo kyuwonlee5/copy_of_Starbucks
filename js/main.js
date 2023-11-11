ceaf9435b75d5654f0c61037148b86e371560353
kyuwonlee5 <kyuwonlee5@gmail.com>
2023-11-12 02:37:29 +0900

    making Starbucks on 23.11.12

--- js/main.js
+++ js/main.js
@@ -35,3 +35,38 @@ window.addEventListener('scroll', _.throttle(function(){
   }
 }, 300));
 //_.throttle(실행할 함수, 시간)
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index){
+  // gsap.to(요소, 지속시간, 옵션);
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  });
+});
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper', {
+  direction: 'vertical',
+  autoplay : true,
+  loop : true
+});
+
+new Swiper('.promotion .swiper', {
+  // 기본값 = horizontal
+  slidesPerView: 3,
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호
+    clickable: true // 사용자가 페이지번호 클릭으로 제어 가능
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
