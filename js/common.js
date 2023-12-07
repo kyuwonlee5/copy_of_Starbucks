9d190d99e1793a3a363f781a8c766267ab472b27
kyuwonlee5 <kyuwonlee5@gmail.com>
2023-12-07 11:59:38 +0900

    공통모듈 분리

---
+++ js/common.js
@@ -0,0 +1,21 @@
+// 공통적으로 사용되는 js
+
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input'); // search class 안에있는 input 요소
+
+searchEl.addEventListener('click', function() {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function(){
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function(){
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
